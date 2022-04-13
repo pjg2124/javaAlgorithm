062f93f9714b5cdb2cbcd03f4b83a69fb519112e
pjg2124 <pjg2124@naver.com>
2022-04-13 15:26:23 +0900

    1330

---
+++ algorithm/Beakjoon1330.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Beakjoon1330 {
+    public static void main(String[] args) {
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (A > B)
+            System.out.println(">");
+        else if (A < B)
+            System.out.println("<");
+        else
+            System.out.println("==");
+    }
+}
