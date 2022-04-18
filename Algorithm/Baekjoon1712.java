46949cea85ead8453d1fec6a7640727e3aebc8db
pjg2124 <pjg2124@naver.com>
2022-04-18 22:51:59 +0900

    1712

---
+++ Algorithm/Baekjoon1712.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Baekjoon1712 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        sc.close();
+
+        if (B >= C) {
+            System.out.println("-1");
+        } else {
+            System.out.println((A / (C-B)) + 1);
+        }
+    }
+}
