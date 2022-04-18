42befe1c564612518e6f5e07b22b1fd45440bff8
pjg2124 <pjg2124@naver.com>
2022-04-18 18:34:09 +0900

    2742

---
+++ Algorithm/Baekjoon2742.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjoon2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+        int num = 0;
+
+        for (int i = testCase; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
