cb02f2e857ba9b4eb39c730e97be8a4fd2b787da
pjg2124 <pjg2124@naver.com>
2022-04-18 16:22:55 +0900

    10950

---
+++ Algorithm/Baekjoon10950.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Baekjoon10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+        int arr[] = new int[testCase];
+
+        for (int i = 0; i < testCase; i++) {
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            arr[i] = num1 + num2;
+        }
+        for (int j = 0; j < testCase; j++) {
+            System.out.println(arr[j]);
+        }
+    }
+}
