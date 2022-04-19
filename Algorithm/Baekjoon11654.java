34a67711c75389b1b80a692daa797033d44df9d4
pjg2124 <pjg2124@naver.com>
2022-04-19 20:49:58 +0900

    11654

---
+++ Algorithm/Baekjoon11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Baekjoon11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.nextLine().charAt(0);
+        sc.close();
+
+        int in = (int)ch;
+        System.out.println(in);
+    }
+}
