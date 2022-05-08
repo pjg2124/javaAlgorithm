b07d1d64dac809f3285b400c691e9a06084639cd
pjg2124 <pjg2124@naver.com>
2022-05-08 13:55:53 +0900

    1001

--- Algorithm/Baekjoon1001.java
+++ Algorithm/Baekjoon1001.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Baekjoon11654 {
+public class Baekjoon1001 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char ch = sc.nextLine().charAt(0);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
         sc.close();
 
-        int in = (int)ch;
-        System.out.println(in);
+        System.out.println(A - B);
     }
 }
